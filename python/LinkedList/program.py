8f6931b7f2ca589dc10ae32c62231a1be44fbd11
chakane3 <39193557+chakane3@users.noreply.github.com>
2022-01-27 11:33:10 -0500

    begin tree

--- python/LinkedList/program.py
+++ python/LinkedList/program.py
@@ -1,12 +1,11 @@
-# Our node is has 2 properties, a value, and a reference to next
-from email import header
-
-
+# Our node is has 2 properties when created, a value, and a reference to next
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 
+# Our linked list has 3 properties when created
+# a head, tail, and the size of the list
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -38,31 +37,20 @@ class LinkedList:
             self.tail.next = newNode
             self.tail = newNode
             self.size += 1
-
-
-    # stops at the n-1 index inside the while
-    def printList(self):
-        currentNode = self.head
-        while currentNode != None:
-            print(currentNode.value, end=" ")
-            currentNode = currentNode.next
-        print()
         
-
     def addNodeAfterIndex(self, value, index):
         newNode = Node(value)
         currentNode = self.head
-
-        if self.isEmpty():
+        # check if our list is empty
+        if self.isEmpty == True:
             self.pushNode(value)
             return
 
         # check if the given index would be out of bounds
-        if index < 0:
-            self.pushNode(value)
         elif index > self.size:
             self.appendNode(value)
         else:
+
             for index in range(0, index):
                 currentNode = currentNode.next
 
@@ -71,16 +59,22 @@ class LinkedList:
             self.size += 1
 
     def addNodeBeforeIndex(self, value, index):
-        newNode = Node(value)
-        currentNode = self.head
-
-        if self.isEmpty() or index <= 0:
+        if self.isEmpty == True or index <= 0:
             self.pushNode(value)
             self.size += 1
             return
 
-        if index-1 > self.size:
-            self.appendNode(value)
+        newNode = Node(value)
+        currentNode = self.head   
+
+        if index >= self.size:
+            for index in range(0, self.size-3):
+                currentNode = currentNode.next
+            newNode.next = currentNode.next
+            currentNode.next = newNode
+            self.size += 1
+            return
+            
         else:
             for index in range(0, index-1):
                 currentNode = currentNode.next
@@ -89,7 +83,7 @@ class LinkedList:
             self.size += 1
 
     def deleteNodeAtIndex(self, index):
-        if self.isEmpty():
+        if self.isEmpty == True:
             print("No elements in list, try adding some")
             return
         if index > self.size or index < 0:
@@ -102,47 +96,50 @@ class LinkedList:
             self.size -= 1
             return
 
+        print("hi")
         for idx in range(0, index-1):
             if currentNode.next != None:
                 currentNode = currentNode.next
-
-
-        currentNode = currentNode.next.next
+        print("The node to delete is: ", currentNode.next.value)
+        print("The current node were on is: ", currentNode.value)
+        currentNode.next = currentNode.next.next
         self.size -= 1
+        return
                 
 
     def isEmpty(self):
         if self.head == None:
             return True
+        return False
+
+    # stops at the n-1 index inside the while
+    def printList(self):
+        currentNode = self.head
+        while currentNode != None:
+            print(currentNode.value, end=" ")
+            currentNode = currentNode.next
+        print()
             
 L = LinkedList()
 L.pushNode(10)
-L.pushNode(9)
-L.pushNode(8)
 L.appendNode(12)
 L.pushNode(5)
 L.addNodeAfterIndex(6, 0)
-L.addNodeAfterIndex(15, 99)
 L.addNodeAfterIndex(7, 1)
 L.addNodeAfterIndex(13, L.size-2)
 
-L.pushNode(-5)
-L.appendNode(22)
-L.addNodeBeforeIndex(2, 1)
-L.addNodeBeforeIndex(3, 2)
-L.addNodeAfterIndex(-4, 0)
-L.addNodeBeforeIndex(25, 13)
+
+L.addNodeBeforeIndex(9, 3)
+
+L.addNodeBeforeIndex(0, 0)
+L.addNodeBeforeIndex(19, 90)
+L.addNodeBeforeIndex(11, 6)
+L.addNodeAfterIndex(4, 0)
+
 L.deleteNodeAtIndex(0)
 L.deleteNodeAtIndex(1)
-
-
-
-
+L.deleteNodeAtIndex(2)
 
 L.printList()
-
 print("The head is: ", L.head.value)
 print("The tail is: ", L.tail.value)
-
-
-
---
+++ swift/Trees/1_intro.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+/* Binary Trees
+
+Each node has a root value and a list of references to other nodes 
+called child nodes (left and right child nodes). We view a tree as 
+having N nodes and N-1 edges (references).
+
+*/
+
+
+public class Node {
+    var leftChild: Node?
+    var rightChild: Node?
+    var value: Int?
+    init(_ value: Int) {
+        self.value = value
+    }
+}
+
+struct BinaryTree {
+    var head: Node?
+    var leftChild: Node?
+    var rightChild: Node?
+    var size = 0
+    init() {}
+}
