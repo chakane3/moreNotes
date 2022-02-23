d659332ced527a88ca1f27da17ca32cd51fb79d3
chakane <39193557+chakane3@users.noreply.github.com>
2022-02-23 04:41:36 -0500

    flask app

--- Web_Development/JS-basics/notes/script.js
+++ Web_Development/JS-basics/notes/script.js
@@ -14,10 +14,15 @@ console.log("Hello, now we can get to rockin'!")
 // const rapper = document.getElementById("lastPara")
 // rapper.textContent += `, ${userSubmittedName}`
 
-const addNameToHeading = () => {
-    const nameInputElement = document.getElementById("name-input-text")
-    const userSubmittedName = nameInputElement.value
-    const topHeadingElement = document.getElementById("top-heading")
-    topHeadingElement.textContent = `My website: Made especially for ${userSubmittedName}`
-}
+// const addNameToHeading = () => {
+//     const nameInputElement = document.getElementById("name-input-text")
+//     const userSubmittedName = nameInputElement.value
+//     const topHeadingElement = document.getElementById("top-heading")
+//     topHeadingElement.textContent = `My website: Made especially for ${userSubmittedName}`
+// }
+
+// create new elements in the DOM
+let newP = document.createElement("p")
+newP.textContent = "YB diss song goated"
+document.body.appendChild(newP)
 
