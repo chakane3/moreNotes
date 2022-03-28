c2fee412bf3ff5a0c3e89175a60484b0929aab87
chakane <39193557+chakane3@users.noreply.github.com>
2022-03-28 13:34:29 -0400

    continue java LL

--- NJIT_java/LinkedList/myLinkedList.java
+++ NJIT_java/LinkedList/myLinkedList.java
@@ -21,9 +21,37 @@ public class myLinkedList {
         }
     }
 
+    public void appendNode(int num) {
+        Node newNode = new Node(num);
+        Node tempNode = head;
+        if(head == null) {
+            head = newNode;
+        }
+        while(tempNode.next != null) {
+            tempNode = tempNode.next;
+        }
+        tempNode.next = newNode;
+    }
+
+    public void pushAtIndex(int num, int index) {
+        Node newNode = new Node(num);
+        Node tempNode = head;
+        if(head == null) {
+            head = newNode;
+        }
+        for(int i = 0; i < index-1; i++) {
+            tempNode = tempNode.next;
+        }
+        // were at 7 here
+        Node tmpRef = tempNode.next;
+        tempNode.next = newNode;
+        newNode.next = tmpRef;
+        
+    }
+
     public void printList() {
         Node temp = head;
-        while(head.next != null) {
+        while(temp.next != null) {
             System.out.print(temp.val + " ");
             temp = temp.next;
         }
@@ -33,6 +61,10 @@ public class myLinkedList {
     public static void main(String[] args) {
         myLinkedList l = new myLinkedList();
         l.pushNode(10);
+        l.pushNode(7);
+        l.pushNode(5);
+        l.appendNode(1);
+        l.pushAtIndex(2, 0);
         l.printList();
     }
 }
