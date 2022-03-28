866ff8ed49fefe2411159fd51aa2194bf34648af
chakane <39193557+chakane3@users.noreply.github.com>
2022-03-28 16:37:29 -0400

    java ll

--- NJIT_java/LinkedList/myLinkedList.java
+++ NJIT_java/LinkedList/myLinkedList.java
@@ -2,6 +2,7 @@ package NJIT_java.LinkedList;
 
 public class myLinkedList {
     Node head;
+    int size = 0;
 
     static class Node {
         int val;
@@ -15,9 +16,11 @@ public class myLinkedList {
         Node newNode = new Node(num);
         if(head == null) {
             head = newNode;
+            size++;
         } else {
             newNode.next = head;
             head = newNode;
+            size++;
         }
     }
 
@@ -26,19 +29,30 @@ public class myLinkedList {
         Node tempNode = head;
         if(head == null) {
             head = newNode;
+            size++;
+            return;
         }
         while(tempNode.next != null) {
             tempNode = tempNode.next;
         }
         tempNode.next = newNode;
+        size++;
     }
 
     public void pushAtIndex(int num, int index) {
         Node newNode = new Node(num);
         Node tempNode = head;
-        if(head == null) {
-            head = newNode;
+
+        if(index <= 0 || head == null) {
+            pushNode(num);
+            return;
         }
+
+        if(index >= size) {
+            appendNode(num);
+            return;
+        }
+
         for(int i = 0; i < index-1; i++) {
             tempNode = tempNode.next;
         }
@@ -46,7 +60,52 @@ public class myLinkedList {
         Node tmpRef = tempNode.next;
         tempNode.next = newNode;
         newNode.next = tmpRef;
+        size++;
         
+    }
+
+    public void deleteAtIndex(int index) {
+        Node tmpNode = head;
+        if(head == null) {
+            return;
+        }
+
+        if(index == 0) {
+            head = head.next;
+            size--;
+            return;
+        }
+
+        if(index >= size || index <= 0) {
+            System.out.println(size);
+            System.out.println("out of bounds index was given");
+            return;
+        }
+        
+        for(int i = 0; i < index-1; i++) {
+            tmpNode = tmpNode.next;
+        }
+        System.out.println("tmNode last: " + tmpNode.val);
+        tmpNode.next = tmpNode.next.next;
+        size--;
+    }
+
+    public int get(int index) {
+        Node tmpNode = head;
+        if(index > size || index <= 0) {
+            System.out.println("out of bounds index was given");
+            return -9999;
+        }
+
+        if(head == null) {
+            return tmpNode.val;
+        }
+
+        for(int i = 0; i < index; i++) {
+            tmpNode = tmpNode.next;
+        }
+        return tmpNode.val;
+
     }
 
     public void printList() {
@@ -64,7 +123,11 @@ public class myLinkedList {
         l.pushNode(7);
         l.pushNode(5);
         l.appendNode(1);
-        l.pushAtIndex(2, 0);
+        l.pushAtIndex(2, -1);
         l.printList();
+        l.deleteAtIndex(3);
+        l.printList();
+        int res = l.get(2);
+        System.out.println(res);
     }
 }
