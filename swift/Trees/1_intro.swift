9995d882ab6cd6642b3321f05682f68b115e52f6
chakane <39193557+chakane3@users.noreply.github.com>
2022-03-30 09:40:00 -0400

    pre,in, post order traversals

--- swift/Trees/1_intro.swift
+++ swift/Trees/1_intro.swift
@@ -47,7 +47,7 @@ extension Node {
         // go left if the inserted value is greater than the current nodes value
         if value < self.value {
             if leftChild != nil {
-                // keep calling insert recursivelt on our left node
+                // keep calling insert on our left node
                 leftChild!.insert(value)
             } else {
                 // create a new node and attach as left child
@@ -55,7 +55,7 @@ extension Node {
             }
         } else {
             if rightChild != nil {
-                // call insert recursively on the right child
+                // call insert on the right child
                 rightChild!.insert(value)
             } else {
                 rightChild = Node(value)
@@ -63,22 +63,80 @@ extension Node {
         }
     }
 
+    /*
+        lnr: visit left -> node -> right
+                    10              output: 1, 6, N, 7, 8, 9, 10
+                   /  \                     N, 11, 14, 20, 22
+                  7   11                
+                /  \  /  \
+               6   8  N   20
+             /  \   \   /  \
+            1   N    9  14  22 
+    */
     func inOrderTraversal() {
         leftChild?.inOrderTraversal()
         print("\(value)", terminator: " ")
         rightChild?.inOrderTraversal()
+    }
+
+    /*
+        nlr: visit node -> left -> right
+                    10              output: 10, 7, 6, 1, N, 8, 
+                   / \                      9, 11, N, 20, 14, 22
+                  7   11                
+                /  \  /  \
+               6   8  N   20
+             /  \   \   /  \
+            1   N    9  14  22 
+    */
+    func preOrderTraversal() {
+        print("\(value)", terminator: " ")
+        leftChild?.preOrderTraversal()
+        
+        rightChild?.preOrderTraversal()
+        
+        
+    }
+
+    /*
+        lrn: left -> right -> node
+                    10             
+                   /  \                      
+                  7   11                
+                /  \  /  \
+               6   8  N   20
+             /  \   \   /  \
+            1   N    9  14  22 
+    */
+    func postOrderTraversal() {
+        leftChild?.postOrderTraversal()
+        rightChild?.postOrderTraversal()
+        print("\(value)", terminator: " ")
 
     }
 }
 
-let rootNode = Node(12)
-rootNode.insert(5)
+let rootNode = Node(10)
+rootNode.insert(7)
+rootNode.insert(6)
+rootNode.insert(1)
+rootNode.insert(9)
+rootNode.insert(8)
+rootNode.insert(11)
+rootNode.insert(20)
 rootNode.insert(14)
-rootNode.insert(34)
-rootNode.insert(0)
-rootNode.insert(13)
+rootNode.insert(22)
 
+
+
+print("in-order: ", terminator: "")
 rootNode.inOrderTraversal()
+print()
+print("pre-order: ", terminator: "")
+rootNode.preOrderTraversal()
+print()
+print("post-order: ", terminator: "")
+rootNode.postOrderTraversal()
 
 
 
